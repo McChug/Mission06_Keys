f979b30d15ec4f3212c003c84422aa6ba1307584
McChug <mcchug2112@gmail.com>
2025-02-10 16:00:11 -0700

    Add project files.

---
+++ Mission06_Keys/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_Keys.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
